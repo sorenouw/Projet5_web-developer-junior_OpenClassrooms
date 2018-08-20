3eaa3de55984f2d737960e2216968f92a7a248b7
Soren Lepera <soren.lepera@gmail.com>
2018-08-20 17:47:23 +0200

    Add image with tinymce

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -35,7 +35,48 @@
 
         <!-- tinymce -->
         <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
-        <script>tinymce.init({ selector:'.editor' });</script>
+        <script>
+        tinymce.init({
+            selector: '.editor',
+            plugins: 'image code advlist emoticons lists textcolor colorpicker',
+            toolbar: 'undo redo | image code | numlist bullist  forecolor backcolor emoticons',
+
+            // without images_upload_url set, Upload tab won't show up
+            images_upload_url: 'upload.php',
+
+            // override default upload handler to simulate successful upload
+            images_upload_handler: function (blobInfo, success, failure) {
+                var xhr, formData;
+
+                xhr = new XMLHttpRequest();
+                xhr.withCredentials = false;
+                xhr.open('POST', 'upload.php');
+
+                xhr.onload = function() {
+                    var json;
+
+                    if (xhr.status != 200) {
+                        failure('HTTP Error: ' + xhr.status);
+                        return;
+                    }
+
+                    json = JSON.parse(xhr.responseText);
+
+                    if (!json || typeof json.location != 'string') {
+                        failure('Invalid JSON: ' + xhr.responseText);
+                        return;
+                    }
+
+                    success(json.location);
+                };
+
+                formData = new FormData();
+                formData.append('file', blobInfo.blob(), blobInfo.filename());
+
+                xhr.send(formData);
+            },
+        });
+        </script>
 
     </body>
 </html>
